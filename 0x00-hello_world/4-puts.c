be51e507d233712a6d963e1afb54e0589e4ec215
mayordefi <victormaxwell61@gmail.com>
2023-06-17 10:13:51 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints "Programming is like building a multilingual puzzle
+ */
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+/**
+  * Return: 1 if the number is positive. 0 otherwise
+*/
+	return (0);
+}
