ad77440f6105ba86179156149e5007130ba2b1fa
mayordefi <victormaxwell61@gmail.com>
2023-06-17 10:24:28 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,14 @@
 /**
  * main - prints "Programming is like building a multilingual puzzle
  */
-int main(void)
+void  main(void)
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
 
 /**
+  * is_positive - Check if a number is greater than 0
+  *
   * Return: 1 if the number is positive. 0 otherwise
 */
 	return (0);
