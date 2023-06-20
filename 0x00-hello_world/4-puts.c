51006ad0bcb4e2785fcab1085f8272acfccca6d6
mayordefi <victormaxwell61@gmail.com>
2023-06-19 21:58:15 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
- * main - prints "Programming is like building a multilingual puzzle
+ * main - Entry point
+ *
+ * main: prints "Programming is like building a multilingual puzzle
  */
 int  main(void)
 {
